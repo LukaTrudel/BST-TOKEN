822fa2c6db5d8769b785142014cd502fce4fe3c7
Luka <luka.trudel@gmail..com>
2022-02-02 21:45:28 -0500

    token name

--- BST.sol
+++ BST.sol
@@ -31,7 +31,7 @@ contract SafeMath {
 }
 
 
-contract BallSackToken is ERC20Interface, SafeMath {
+contract EOWJOHAN is ERC20Interface, SafeMath {
     string public name;
     string public symbol;
     uint8 public decimals; // 18 decimals is the strongly suggested default, avoid changing it
@@ -47,8 +47,8 @@ contract BallSackToken is ERC20Interface, SafeMath {
      * Initializes contract with initial supply tokens to the creator of the contract
      */
     constructor() public {
-        name = "BallSackToken";
-        symbol = "BALLz";
+        name = "EOWJOHAN";
+        symbol = "EOWJOHAN";
         decimals = 18;
         _totalSupply = 100000000000000000000000000;
 
